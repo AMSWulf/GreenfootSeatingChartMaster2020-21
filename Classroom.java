890514530277a9b07b32897f7cf4a59f9f04d319
Lukas Somwong <93167066+ItsJustLukas78@users.noreply.github.com>
2023-08-30 17:24:28 -0700

    adding my animated student actor
    Signed-off-by: Lukas Somwong <93167066+ItsJustLukas78@users.noreply.github.com>

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        LukasSomwong lukassomwong = new LukasSomwong();
+        addObject(lukassomwong,5,7);
+        lukassomwong.assignSeat();
   
     }
     
