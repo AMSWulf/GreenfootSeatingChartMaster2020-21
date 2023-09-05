025effc9e1dcfa172490ece233e52590836b2124
ShreyaShingare <shingare.shreya@gmail.com>
2023-09-05 10:23:19 -0700

    more changes

--- Classroom.java
+++ Classroom.java
@@ -152,6 +152,10 @@ public class Classroom extends World
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
+        
+        ShreyaShingare shreyashingare = new ShreyaShingare();
+        addObject(shreyashingare,5,6);
+        shreyashingare.assignSeat();
   
     }
     
