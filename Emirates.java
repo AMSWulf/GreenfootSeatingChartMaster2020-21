5c89d81b8de2eef435a83aa06831aad86be23937
Anilkumar Sarvabatla <71952099+Mybobksf@users.noreply.github.com>
2023-09-18 10:45:33 -0700

    Update Emirates.java to add the image of the plane

--- Emirates.java
+++ Emirates.java
@@ -12,6 +12,9 @@ public class Emirates extends FlyingObjects
      * Act - do whatever the Emirates wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public Emirates(){
+        setImage("Emirates.png");
+    }
     /*public void act()
     {
         // Add your action code here.
