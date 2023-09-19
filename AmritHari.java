18e44884445f1e882adff0e2a42a4c6b34436b3a
amrit42069 <142340934+amrit42069@users.noreply.github.com>
2023-09-19 07:47:35 +0530

    Create AmritHari.java

---
+++ AmritHari.java
@@ -0,0 +1,36 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+
+public class AmritHari extends Student implements SpecialInterestOrHobby
+{
+
+    /**
+     * Constructor for the AshmitSethi class.
+     * Constructors are special methods with the same exact name as the class name.  
+     * Constructors to not have return types.
+     * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
+     *  lists to initalize for different conditions (depending on what constructors have been written.
+     * @param String f (firstname)
+     * @param String l (lastname)
+     * @param int r (row of seating arrangement)
+     * @param int s (seat number within row seating arrangement)
+     * 
+     */
+    public AmritHari() {
+        portraitFile="AmritHari.png";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
+        setImage(portraitFile);
+    }
+    public void act(){
+      if(Greenfoot.mouseClicked(this)){
+                getName();
+                sayName("AmritHari_seating_chart.m4a");
+                myHobby();
+      }
+    }
+    public void getName(){
+        System.out.println("My name is Amrit Hari");
+    }
+    public void myHobby() {
+         System.out.println("I like coding");
+    }
+}
