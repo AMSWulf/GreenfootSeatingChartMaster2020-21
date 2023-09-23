057d73bffca9b656ed2a2f22b4bb4a9c954f95c3
amrit42069 <142340934+amrit42069@users.noreply.github.com>
2023-09-22 21:22:25 -0700

    Update AmritHari.java

--- AmritHari.java
+++ AmritHari.java
@@ -1,36 +1,67 @@
-// import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+ import greenfoot.*; 
+
+ public class AmritHari extends Student implements SpecialInterestOrHobby
+ {
+     public AmritHari(String f, String l, int r, int s) {
+         firstName - f;
+         lastName = l;
+         mySeatX = r;
+         mySeatY = s;
+         portraitFile = f.tolowerCase()+ l.toLowerCase()+ ".jpg";
+         standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+         soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
+         setImage(portraitFile);
+         sitting=true;
+     }
+     public AmritHari() {
+         firstName = "Amrit";
+         lastName = "Hari";
+         mySeatX = 1;
+         mySeatY = 1;
+         portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
+         standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+         soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav";
+         setImage(portraitFile);
+         sitting=true;
+     }
+     public void act() 
+     {
+         if(Greenfoot.mouseClicked(this)){
+             sitting=false;
+             setImage(standingFile);
+             System.out.println("");
+             getName();
+             sayName(soundFile);
+             myHobby("I like to read");
+             diagonalMovement();  
+             sitDown();
+            }
+        
+    }
+     public void getName(){
+        System.out.println("My name is " + firstName + " " + lastName);
+    }
+public void diagonalMovement() {
+    setLocation(0, 0);
+    Greenfoot.delay(10);
+
+    for (int i = 1; i <= 9; i++) {
+        setLocation(i, i);
+        Greenfoot.delay(10);
+    }
+
+    for (int i = 9; i >= 0; i--) {
+        setLocation(i, 5 - i);
+        Greenfoot.delay(10);
+    }
+
+    Greenfoot.delay(20);
+    returnToSeat();
+}
+     public void myHobby(String s) {
+         System.out.println(s);
+}
 
 
-// public class AmritHari extends Student implements SpecialInterestOrHobby
-// {
-
-    // /**
-     // * Constructor for the AshmitSethi class.
-     // * Constructors are special methods with the same exact name as the class name.  
-     // * Constructors to not have return types.
-     // * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
-     // *  lists to initalize for different conditions (depending on what constructors have been written.
-     // * @param String f (firstname)
-     // * @param String l (lastname)
-     // * @param int r (row of seating arrangement)
-     // * @param int s (seat number within row seating arrangement)
-     // * 
-     // */
-    // public AmritHari() {
-        // portraitFile="AmritHari.png";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
-        // setImage(portraitFile);
-    // }
-    // public void act(){
-      // if(Greenfoot.mouseClicked(this)){
-                // getName();
-                // sayName("AmritHari_seating_chart.m4a");
-                // myHobby();
-      // }
-    // }
-    // public void getName(){
-        // System.out.println("My name is Amrit Hari");
-    // }
-    // public void myHobby() {
-         // System.out.println("I like coding");
-    // }
-// }
+     
+ }
